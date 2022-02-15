f9de4967528b8df3bcfadf1deda7c411323722d3
UchihaCryptic <71690828+UchihaCryptic@users.noreply.github.com>
2022-02-14 23:51:32 -0800

    Changed structure of "in-a-row" counting with separate function calls

--- Connect4-AI-Project/players.py
+++ Connect4-AI-Project/players.py
@@ -80,28 +80,126 @@ class stupidAI(connect4Player):
 			move[:] = [0]
 
 class minimaxAI(connect4Player):
-
-	def in_a_row_counter(self, env):
-		print("no")
 	
-	def eval(self, env, move):
+
+	def backSlashDiag(self, board, row, col):
+		count = 1
+		temp_row = row
+		temp_col = col
+		while(temp_row-1 > -1 and temp_col-1 > -1 and board[temp_row-1][temp_col-1] == 1):
+			count += 1
+			temp_row -= 1
+			temp_col -= 1
 		
-		my_fours = opp_fours = my_threes = opp_threes = my_twos = opp_twos = my_ones = opp_ones = 0
+		temp_row = row
+		temp_col = col
+		while(temp_row+1 < 6 and temp_col+1 < 7 and board[temp_row+1][temp_col+1] == 1):
+			count += 1
+			temp_row += 1
+			temp_col += 1
 		
-		fours = my_fours - opp_fours
-		if (fours > 0):
-			print("Value of game: ", inf)
-			return inf
-		elif (fours < 0):
-			print("Value of game: ", -inf)
-			return -inf
-		else:
-			threes = 10000*(my_threes - opp_threes)
-			twos = 100*(my_twos - opp_twos)
-			ones = my_ones - opp_ones
-			val = threes + twos + ones
-			print("Value of game: ", val)
-		return val
+		return count
+	
+	def forwardSlashDiag(self, board, row, col):
+		count = 1
+		temp_row = row
+		temp_col = col
+		while(temp_row-1 > -1 and temp_col+1 < 7 and board[temp_row-1][temp_col+1] == 1):
+			count += 1
+			temp_row -= 1
+			temp_col += 1
+		
+		temp_row = row
+		temp_col = col
+		while(temp_row+1 < 6 and temp_col-1 > -1 and board[temp_row+1][temp_col-1] == 1):
+			count += 1
+			temp_row += 1
+			temp_col -= 1
+		
+		return count
+	
+	def vertical(self, board, row, col):
+		count = 1
+		temp_row = row
+		while(temp_row+1 < 6 and board[temp_row+1][col] == 1):
+			count += 1
+			temp_row += 1
+		
+		temp_row = row
+		while(temp_row-1 > -1 and board[temp_row-1][col] == 1):
+			count += 1
+			temp_row -= 1
+		
+		return count
+	
+	def horizontal(self, board, row, col):
+		count = 1
+		temp_col = col
+		while(temp_col+1 < 7 and board[row][temp_col+1] == 1):
+			count += 1
+			temp_col += 1
+		
+		temp_col = col
+		while(temp_col-1 > -1 and board[row][temp_col-1] == 1):
+			count += 1
+			temp_col -= 1
+		
+		return count
+
+	def eval(self, env):
+		copy = deepcopy(env)
+		ones = twos = threes = fours = 0
+		
+		for row in range(6):
+			for col in range(7):
+				if copy.board[row][col] == 1:
+					print("I'm in player 1")
+					#check diagonals
+					val = self.backSlashDiag(copy.board,row,col)
+					if val == 1:
+						ones += 1
+					elif val == 2:
+						twos += 1
+					elif val == 3:
+						threes += 1
+					elif val == 4:
+						fours += 1
+					val = self.forwardSlashDiag(copy.board,row,col)
+					if val == 1:
+						ones += 1
+					elif val == 2:
+						twos += 1
+					elif val == 3:
+						threes += 1
+					elif val == 4:
+						fours += 1
+					#check horizontal
+					val = self.horizontal(copy.board,row,col)
+					if val == 1:
+						ones += 1
+					elif val == 2:
+						twos += 1
+					elif val == 3:
+						threes += 1
+					elif val == 4:
+						fours += 1
+					#check vertical
+					val = self.vertical(copy.board,row,col)
+					if val == 1:
+						ones += 1
+					elif val == 2:
+						twos += 1
+					elif val == 3:
+						threes += 1
+					elif val == 4:
+						fours += 1
+					print("This is how many ones: ", ones, " twos: ", twos, " threes: ", threes, " fours: ", fours)
+				elif copy.board[row][col] == 2:
+					print("I'm in player 2")
+					#check diagonals
+					#check horizontal
+					#check vertical
+
 	
 	def play(self, env, move):
 
@@ -122,104 +220,7 @@ class minimaxAI(connect4Player):
 		else:
 			move[:] = [0]
 		
-		# INSERT BS HERE
-		copy = deepcopy(env)
-
-		for row in range(6):
-			for col in range(7):
-				#Initializations
-				downSlopeLTRCount = upSlopeLTRCount = upDownCount = horizontalCount = 0
-				temp = 0
-				ones = twos = threes = fours = 0
-				count2 = 0
-				
-				if copy.board[row][col] == 1:
-					#Checking for in a row from down slope left to right
-					while(row-1 > -1 and col-1 > -1 and copy.board[row-1][col-1] == 1):
-						temp += 1
-						print("downSlopeLTR row- col-")
-					downSlopeLTRCount = temp + 1
-					temp = 0
-					while(row+1 < 6 and col+1 < 7 and copy.board[row+1][col+1] == 1):
-						temp += 1
-						print("downSlopeLTR row+ col+")
-					downSlopeLTRCount = downSlopeLTRCount + temp
-
-					#Now checking for in a row from up slope left to right
-					while(row-1 > -1 and col+1 < 7 and copy.board[row-1][col+1] == 1):
-						temp += 1
-					upSlopeLTRCount = temp + 1
-					temp = 0
-					while(row+1 < 6 and col-1 > -1 and copy.board[row+1][col-1] == 1):
-						temp += 1
-					upSlopeLTRCount = upSlopeLTRCount + temp
-					
-					#Now checking for in a col up and down
-					temp = 0
-					while(row+1 < 6 and copy.board[row+1][col] == 1):
-						temp += 1
-					upDownCount = temp + 1
-					temp = 0
-					while(row-1 > -1 and copy.board[row-1][col] == 1):
-						temp += 1
-					upDownCount = upDownCount + temp
-
-					#Now checking for in a row side to side
-					temp = 0
-					while(col+1 < 7 and copy.board[row][col+1] == 1):
-						temp += 1
-					horizontalCount = temp + 1
-					temp = 0
-					while(col-1 > -1 and copy.board[row][col-1] == 1):
-						temp += 1
-					horizontalCount = horizontalCount + temp
-
-					#Tally results
-					if downSlopeLTRCount == 1:
-						ones = ones + downSlopeLTRCount
-					elif downSlopeLTRCount == 2:
-						twos = twos + downSlopeLTRCount
-					elif downSlopeLTRCount == 3:
-						threes = threes + downSlopeLTRCount
-					else:
-						fours = fours + downSlopeLTRCount
-					
-					if upSlopeLTRCount == 1:
-						ones = ones + upSlopeLTRCount
-					elif upSlopeLTRCount == 2:
-						twos = twos + upSlopeLTRCount
-					elif upSlopeLTRCount == 3:
-						threes = threes + upSlopeLTRCount
-					else:
-						fours = fours + upSlopeLTRCount
-
-					if upDownCount == 1:
-						ones = ones + upDownCount
-					elif upDownCount == 2:
-						twos = twos + upDownCount
-					elif upDownCount == 3:
-						threes = threes + upDownCount
-					else:
-						fours = fours + upDownCount
-
-					if horizontalCount == 1:
-						ones = ones + horizontalCount
-					elif horizontalCount == 2:
-						twos = twos + horizontalCount
-					elif horizontalCount == 3:
-						threes = threes + horizontalCount
-					else:
-						fours = fours + horizontalCount
-					
-				elif copy.board[row][col] == 2:
-					count2 += 1
-					print("Player 2 move at: Row ", row, " Col ", col, " (TRUE INDEXED)")
-				else:
-					print("blank")#idk wtf
-
-		print("Number of ones: ", ones, " twos: ", twos, " threes: ", threes, "fours: ", fours)
-		print(" Here's count 2: ", count2)
-		# EXIT BS HERE
+		self.eval(env)
 
 	def minimax():
 		print("Chunk")
